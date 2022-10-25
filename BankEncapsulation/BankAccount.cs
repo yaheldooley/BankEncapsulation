e6f05107e493f78a42318202d62a3dc7e379fd37
yahel dooley <ydooley@gmail.com>
2022-10-24 17:38:20 -0700

    Complete Encapsulation Exercise for TrueCoders Bank

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+	public class BankAccount
+	{
+		private double balance = 0;
+
+		public void Deposit(double amount)
+		{
+			Console.WriteLine($"An amount of ${amount} has been deposited.");
+			Console.WriteLine($"\nNote: Please allow 2-3 business days for amount to post to your account.");
+			balance += amount;
+		}
+
+		public double GetBalance()
+		{
+			
+			return balance;
+		}
+	}
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,69 @@
-﻿namespace BankEncapsulation
+﻿using System.Security.AccessControl;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            //account.Deposit(500.48);
+
+            bool active = true;
+            do
+            {
+                Console.WriteLine("Welcome to TrueCoders Bank!\n");
+
+                Console.WriteLine("Enter the number of the action you would like to do and press RETURN key.");
+                Console.WriteLine($"---- 1. See ACCOUNT BALANCE");
+				Console.WriteLine($"---- 2. Make DEPOSIT");
+                int value;
+				if (int.TryParse(Console.ReadLine(), out value))
+                {
+                    switch(value)
+                    {
+                        case 1:
+							Console.WriteLine($"--------------------------------------");
+							Console.WriteLine($"Your pending balance is: ${account.GetBalance()}");
+							Console.WriteLine($"--------------------------------------\n");
+							Console.WriteLine("Press RETURN key to continue...");
+                            Console.ReadLine();
+							break;
+
+                        case 2:
+                            bool depositing = true;
+                            do
+                            {
+								Console.WriteLine($"--------------------------------------");
+								Console.WriteLine($"How much would you like to deposit?");
+								Console.WriteLine($"--------------------------------------\n");
+								Console.WriteLine("Press RETURN key to submit amount...Entering 0 will Exit");
+                                double deposit;
+                                if (double.TryParse(Console.ReadLine(), out deposit))
+                                {
+                                    if (deposit == 0) break;
+                                    else account.Deposit(deposit);
+									Console.WriteLine("Press RETURN key to continue...");
+                                    Console.ReadLine();
+                                    depositing = false;
+								}
+                                else
+                                {
+                                    Console.WriteLine("Invalid number. Press RETURN key to try again.");
+									Console.ReadLine();
+								}
+
+							} while (depositing);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid input. Try again");
+                            break;
+                    }
+                    Console.Clear();
+                }
+            } while (active);
+
         }
     }
 }
